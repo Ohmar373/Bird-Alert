de765c5020260e5897c1a15cc0b8dc5aa4488dfa
adamgarcia03 <adam.garcia03@utrgv.edu>
2026-05-04 23:50:31 -0500

    Updated Settings to test mock website

--- bird_alert/settings.py
+++ bird_alert/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # 3. Convert the DEBUG string from .env to a real Boolean
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 INSTALLED_APPS = [
